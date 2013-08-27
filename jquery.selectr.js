e5695c02b611532e6eeb8f5441e5f2a77ce1e4b4
Blaženko Karešin <karo@mjestopodsuncem.com>
2013-08-27 23:58:59 +0200

    generation from <select>, click events

--- jquery.selectr.js
+++ jquery.selectr.js
@@ -1,28 +1,79 @@
-function enableSelectBoxes() {
-	$('div.selectr').each(function() {
+
+
+(function ($) {
+	
+	var privateX = 0;
+	
+	$.fn.selectr = function(options) {
+		
+		// This is the easiest way to have default options.
+		var settings = $.extend({}, $.fn.selectr.defaults, options);
 		
 		
-		var list = $(this).children('ul.selectr-list');
-		
-		
-		$(this).children('span.selectr-display').html(list.children('li.selectr-option:first').html());
-		$(this).children('input.selectr_value').attr('value',list.children('li.selectr-option:first').attr('data-value'));
-		
-		$(this).children('span.selectr-display,span.selectr-btn').click(function(){
-			if (list.css('display') == 'none'){
-				list.css('display','block');
-			}
-			else {
-				list.css('display','none');
-			}
+		return this.each(function(index) {
+			
+			/// this = select element 
+			var origName = $(this).attr('name');
+			$(this).attr('name', origName + '_original');
+			$(this).hide();
+			
+			
+			var selectr = $("<div class='selectr' id='selectr-"+index+"'/>");
+			$(this).after(selectr);
+			
+			var field = $('<input name="'+origName+'" type="hidden"/>');
+			selectr.append(field);
+			var display = $("<span class='selectr-display'>BBB</span>");
+			selectr.append(display);
+			var btn = $("<span class='selectr-btn'>");
+			selectr.append(btn);
+			var list = $("<ul class='selectr-list' id='selectr-list-"+index+"'/>");
+			selectr.append(list);
+			
+			btn.html(settings.arrowHtml);
+			
+			$('span.selectr-display,span.selectr-btn').click(function(){
+				console.log(list);
+				if (list.css('display') == 'none'){
+					list.css('display','block');
+				}
+				else {
+					list.css('display','none');
+				}
+			});
+			
+			$('option', this).each(function(idx) {
+				
+				/// this = option element 
+				var li = $("<li class='selectr-option' id='selectr-li-"+index+"-"+idx+"'/>");
+				list.append(li);
+				li.attr('data-value', $(this).val());
+				li.text($(this).text());
+				if (idx == 0 || $(this).attr('selected') != null) {
+					display.text($(this).text());
+				}
+				
+				li.click(function(){
+					list.css('display','none');
+					field.val($(this).attr('data-value'));
+					display.text($(this).text());
+				});
+				
+			});
+			
+			
+			
 		});
 		
-		$(this).find('li.selectr-option').click(function(){
-			list.css('display','none');
-			$(this).parent().siblings('input.selectr_value').attr('value',$(this).attr('data-value'));
-			$(this).parent().siblings('span.selectr-display').text($(this).text());
-		});
-		
-	});
-}
+	};
+	
+}( jQuery ));
+
+
+$.fn.selectr.defaults = {
+	arrowHtml : '&#9660;'
+};
+
+
+
 
